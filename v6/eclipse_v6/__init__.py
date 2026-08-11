c3638742fa5f4ee7fae7f2ef19cd8f2453bdf0e4
Josef Slavicek <Josef.Slavicek@jslavicek.cz>
2026-08-11 09:16:13 +0000

    v6 is dead

--- v6/eclipse_v6/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Eclipse stacking pipeline (v6 library). Import submodules from notebooks or scripts."""
-
-from eclipse_v6.device import configure_cuda_visible_devices, require_cuda
-
-__all__ = ["configure_cuda_visible_devices", "require_cuda"]
